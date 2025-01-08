b0b02ecb2f2cbfacb7ce75d117ad468032e3c560
Shmily <shmily@positive.tech>
2025-01-08 16:17:52 -0500

    Added check for file size

--- Bitdefender Quarantine Monitor/Positive - Monitor for Bitdefender Quarantine Reader (SQLite DLL files) [WIN].ps1
+++ Bitdefender Quarantine Monitor/Positive - Monitor for Bitdefender Quarantine Reader (SQLite DLL files) [WIN].ps1
@@ -9,10 +9,15 @@ function write-DRMMAlert ($message) {
     write-host "Alert=$message"
     write-host '<-End Result->'
 }
-$SQLLitePath = "$env:ProgramData\System.Data.SQLite.dll"
-$SQLLiteOldPath = "$env:ProgramData\System.Data.SQLite-Old.dll"
+$SQLLiteName = "System.Data.SQLite.dll"
+$SQLLiteOldName = "System.Data.SQLite-Old.dll"
+$SQLLitePath = "$env:ProgramData\$SQLLiteName"
+$SQLLiteOldPath = "$env:ProgramData\$SQLLiteOldName"
 $SQLLiteDownloadUrl = "https://raw.githubusercontent.com/github-positive/Bitdefender-Quarantine-Monitor/refs/heads/main/System.Data.SQLite.dll"
 $SQLLiteOldDownloadUrl = "https://raw.githubusercontent.com/github-positive/Bitdefender-Quarantine-Monitor/refs/heads/main/System.Data.SQLite-Old.dll"
+$DiagMessages = @()
+$AlertMessage = ""
+$ExitCode = 0
 
 if (!(Test-Path $SQLLitePath)) {
     try {
@@ -28,8 +33,31 @@ if (!(Test-Path $SQLLiteOldPath)) {
         #do nothing
     }
 }
-if ((!(Test-Path $SQLLitePath)) -or (!(Test-Path $SQLLiteOldPath)) ) {
-    write-DRMMDiag "Error: 1 or both of the SQLite DLL files are missing. `nPlease run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file. `nIf that fails you can download them from $SQLLiteDownloadUrl and $SQLLiteOldDownloadUrl and save them as $SQLLitePath and $SQLLiteOldPath"
-    write-DRMMAlert "1 or both of the SQLite DLL files are missing."
-    Exit 1
+$SQLLitePresent = (Test-Path $SQLLitePath -ErrorAction SilentlyContinue)
+$SQLLiteOldPresent = (Test-Path $SQLLiteOldPath -ErrorAction SilentlyContinue)
+$SQLLiteSize = ((Get-Item $SQLLitePath -ErrorAction SilentlyContinue).Length )
+$SQLLiteOldSize = ((Get-Item $SQLLiteOldPath -ErrorAction SilentlyContinue).Length )
+
+
+if ((! $SQLLitePresent) -or (! $SQLLiteOldPresent) ) {
+    $DiagMessages += "Error: 1 or both of the SQLite DLL files are missing. `nPlease run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file. `nIf that fails you can download them from $SQLLiteDownloadUrl and $SQLLiteOldDownloadUrl and save them as $SQLLitePath and $SQLLiteOldPath"
+    $AlertMessage = "1 or both of the SQLite DLL files are missing. "
+    $ExitCode = 1
+}
+
+
+if (($SQLLitePresent -and ($SQLLiteSize -lt 1024000)) -or ($SQLLiteOldPresent -and ($SQLLiteOldSize -lt 1024000) ) ) {
+    $DiagMessages += "Error: 1 or both of the SQLite DLL files are below the expected file size and are likely corrupted or failed the download. `nDownload by running the 'Positive - Add Bitdefender Quarantine Reader [WIN]' component  or from $SQLLiteDownloadUrl and $SQLLiteOldDownloadUrl"
+    $AlertMessage += "1 or both of the SQLite DLL files are missing."
+    $ExitCode = 1
+}
+
+
+if ($ExitCode -eq 0) {
+    write-DRMMAlert "Healthy - Both DLL files are present."
+    Exit $ExitCode
+} else {
+    write-DRMMDiag $DiagMessages
+    write-DRMMAlert $AlertMessage
+    Exit $ExitCode
 }
