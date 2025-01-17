b4cf892ba0e4c82a52be82ca5860540f9618a0fc
Shmily <shmily@positive.tech>
2025-01-17 12:52:33 -0500

    v2

--- Content Filter Test/MonitorContentFilterLogFile.ps1
+++ Content Filter Test/MonitorContentFilterLogFile.ps1
@@ -13,8 +13,13 @@ function write-DRRMAlert ($message) {
 $logFolderPath = Join-Path -Path $env:ProgramData -ChildPath "Positive"
 $logFilePath = Join-Path -Path $logFolderPath -ChildPath "ContentFilteringStatus.log"
 
-$expectedBlockedCategories = "Social Media,Shopping,News,Entertainment,Adult Content"
+$expectedBlockedCategories = $ENV:expectedBlockedCategories
+if ($expectedBlockedCategories.Length -lt 1){
+    $expectedBlockedCategories = "Adult Content"
+}
+$supportedBlockedCategories = "Social Media,Shopping,News,Entertainment,Adult Content"
 $expectedBlockedCategoriesArray = $expectedBlockedCategories -split ','
+$supportedBlockedCategoriesArray = $supportedBlockedCategories -split ','
 
 $alert = $false
 $diagMessages = @()
@@ -24,39 +29,48 @@ $udfNumber = "13"
 # Check if the log file exists
 if (-Not (Test-Path -Path $logFilePath)) {
     Write-Host "Log file not found: $logFilePath"
-    exit 1
-}
+    #exit 1
+} else{
 
-# Read the latest log entry from the log file
-try {
-    $latestLogLine = Get-Content -Path $logFilePath -Tail 1
+    # Read the latest log entry from the log file
+    try {
+        $latestLogLine = Get-Content -Path $logFilePath -Tail 1
 
-    # Write to UDF
-    Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom$udfNumber" -Value $latestLogLine -Force | Out-Null
+        # Write to UDF
+        Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom$udfNumber" -Value $latestLogLine -Force | Out-Null
 
-    # Extract the JSON portion of the log line
-    if ($latestLogLine -match "{.+}") {
-        $jsonContent = $matches[0]
-        $latestResults = $jsonContent | ConvertFrom-Json
-        $results = ""
+        # Extract the JSON portion of the log line
+        if ($latestLogLine -match "{.+}") {
+            $jsonContent = $matches[0]
+            $latestResults = $jsonContent | ConvertFrom-Json
+            $results = ""
 
-        foreach ($category in $expectedBlockedCategoriesArray) {
-            $category = $category.Trim() # Remove any extra spaces
-            if ($null -ne $latestResults.$category -and $latestResults.$category -eq "Allowed") {
-                $alert = $true
-                $results += "$category is Allowed but should be Blocked.`n"
+            foreach ($category in $expectedBlockedCategoriesArray) {
+                $category = $category.Trim() # Remove any extra spaces
+
+                # Check if all categories are supported
+                <#
+                if ($category -inotin $supportedBlockedCategoriesArray){
+                    write-DRMMDiag "An unsupported category found in site variable 'expectedBlockedCategories'."
+                    write-DRRMAlert "An unsupported category found in site variable 'expectedBlockedCategories'."
+                    Exit 1
+                } #>
+                if ($null -ne $latestResults.$category -and $latestResults.$category -eq "Allowed") {
+                    $alert = $true
+                    $results += "$category is Allowed but should be Blocked.`n"
+                }
             }
+            $results += "`nCurrent expected blocked categories: $expectedBlockedCategories"
+            $results += "`nFor exclusions see https://positivecomputers.itglue.com/2538598/docs/18348433#id-bba8bb25-c3d8-4ba7-887f-5c0a5a73a41e"
+            $diagMessages += $results
+        } else {
+            Write-Host "No valid JSON found in the latest log entry."
+            #exit 1
         }
-        $results += "`nCurrent expected blocked categories: $expectedBlockedCategories"
-        $results += "`nTo exclude a category from alerting for this client, adjust the site variable 'expectedBlockedCategories'"
-        $diagMessages += $results
-    } else {
-        Write-Host "No valid JSON found in the latest log entry."
-        exit 1
+    } catch {
+        Write-Host "Error processing the log file or writing the UDF. Error: $_"
+        #exit 1
     }
-} catch {
-    Write-Host "Error processing the log file or writing the UDF. Error: $_"
-    exit 1
 }
 
 if ($alert){
